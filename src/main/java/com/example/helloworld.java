3c4968d61f9214ce35358ba132370c94ee524849
prajyotrakh <131329281+prajyotrakh@users.noreply.github.com>
2023-05-25 16:10:56 +0530

    Update helloworld.java

--- src/main/java/com/example/helloworld.java
+++ src/main/java/com/example/helloworld.java
@@ -1,6 +1,5 @@
-package com.example;
-public class helloworld {
-    public static void main(String args[]){
-      System.out.println("hello world");
-    }
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
   }
+}
